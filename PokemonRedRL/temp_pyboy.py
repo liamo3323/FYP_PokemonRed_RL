b4753487a72243dd5a3168092fb6a739f905296b
liam <liam3323@gmail.com>
2023-12-11 18:57:18 +0800

    started report

--- PokemonRedRL/temp_pyboy.py
+++ PokemonRedRL/temp_pyboy.py
@@ -1,5 +1,14 @@
 from pyboy import PyBoy
-pyboy = PyBoy('PokemonRed.gb')
+import sys
+
+
+pyboy = PyBoy('PokemonRed.gb', debugging=False,
+                disable_input=False,
+                window_type='SDL2',
+                hide_window='--quiet' in sys.argv,)
+
+pyboy.load_state(open("init_episode.state", "rb"))
+
 while not pyboy.tick():
     pass
 pyboy.stop()
